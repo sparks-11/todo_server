37f5c56ac6635c91f98d6c614c46fd843965fbcd
sparks-11 <84897249+sparks-11@users.noreply.github.com>
2022-10-27 22:38:24 +0530

    Add files via upload
    updated code server side

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const task = require("../server/models/task");
 const bodyParser = require("body-parser");
+var cors = require("cors");
 
 const app = express();
 // app.use(bodyParser());
 app.use(express.json());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.listen(7000, () => {
   console.log("running on 7000");
 });
@@ -28,21 +34,49 @@ app.post("/addTask", async (req, res) => {
   const data = req.body;
   console.log(data);
   await task
-    .create(req.data)
-    .then((resonse) => res.send("success"))
+    .create(data)
+    .then((response) => res.send("success"))
     .catch((err) => {
       console.log(err);
-      res.send("not success");
+      res.status(500).send("not success");
     });
 });
 
 app.get("/tasks", async (req, res) => {
   await task
-    .findAll()
-    .then((resonse) => res.json({ data: resonse }))
+    .find()
+    .then((response) => res.send(response))
     .catch((err) => {
       console.log(err);
-      res.send("not success");
+      res.status(500).send("not success");
+    });
+});
+
+app.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  await task
+    .findByIdAndUpdate(
+      { _id: id },
+      {
+        $set: data,
+      }
+    )
+    .then((response) => res.send("success"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("not success");
+    });
+});
+
+app.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  await task
+    .findByIdAndDelete({ _id: id })
+    .then((response) => res.send("success"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("not success");
     });
 });
 
